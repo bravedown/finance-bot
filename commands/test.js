81671c21a5b2d2f31492a4dfe7f43b4faeaee718
David <davidbrown3142@gmail.com>
2020-10-29 16:43:10 -0400

    test working

---
+++ commands/test.js
@@ -0,0 +1,33 @@
+// endpoint http://api.currencylayer.com/
+const axios = require("axios");
+const csv = require("csvtojson");
+
+module.exports = {
+	name: 'test',
+    description: 'Get USDX at a date.',
+    cooldown: 0,
+    execute(message, args) { // omit args argument if not using args
+        let daysAgo = args.length < 1 ? 1 : +args[0];
+        let today = Date.now() / 1000 + 86400;
+        today -= today % 86400;
+        let firstDay = today - (86400 * daysAgo);
+        const queryURL = `https://query1.finance.yahoo.com/v7/finance/download/DX-Y.NYB?period1=${firstDay}&period2=${today}&interval=1d&events=history`;
+        // const queryURL = `https://query1.finance.yahoo.com/v7/finance/download/DX-Y.NYB?range=1y&interval=1d&events=history`;
+
+        axios.get(queryURL)
+            .then(function (response) {
+                csv()
+                .fromString(response.data)
+                .then((arr)=>{ 
+                    let dayOneClose = arr[0].Close;
+                    if (dayOneClose === null) dayOneClose = arr[1].Close;
+                    let finalDayClose = arr[arr.length - 1].Close;
+                    let percentChange = finalDayClose / dayOneClose - 1;
+                    message.channel.send(`Percent change of USDX from ${daysAgo} days ago is ${(percentChange * 100).toFixed(4)}%`);
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+        });
+	},
+};
